3d25387241a679b559b3019bf5f59fd14d25705f
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-05-20 12:12:55 -0300

    feat(monetary/index.js): add getChange function

---
+++ backend/src/monetary/index.ts
@@ -0,0 +1,15 @@
+const calculateChange = (purchasePrice: number, providedValue: number) => {
+  const change = providedValue - purchasePrice;
+  const ratio = (change / 100).toFixed(2);
+  const [integer, decimal] = String(ratio).split('.');
+  const billsQuantity = {
+    '100': Number(integer),
+    '10': Number(decimal.split('')[0]),
+    '1': Number(decimal.split('')[1]),
+  };
+  const total = Object.values(billsQuantity)
+      .reduce((acc, curr) => acc + curr, 0);
+  return {total, billsQuantity};
+};
+
+export default calculateChange;
