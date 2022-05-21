b8ec2219391cdf3ba06815dc578f629c9ab1f278
Carlos Daniel Cabral <dev.carlosdaniel@gmail.com>
2022-05-21 13:44:29 -0300

    feat: add homePage style

--- frontend/src/home/HomePage.tsx
+++ frontend/src/home/HomePage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/image/playground.png';
 import './HomePage.css';
 
@@ -8,6 +9,24 @@ function HomePage() {
       <header>
         <img src={logo} alt="Playground" id="logo" width="600" />
       </header>
+
+      <main>
+        <section className="projects">
+          <Link to="/palindrome">
+            <h2>Palíndromos</h2>
+          </Link>
+
+          <Link to="/monetary">
+            <h2>Caixa</h2>
+          </Link>
+          <Link to="/garage">
+            <h2>Garagem</h2>
+          </Link>
+          <Link to="/ceps">
+            <h2>CEPs</h2>
+          </Link>
+        </section>
+      </main>
     </div>
   );
 }
